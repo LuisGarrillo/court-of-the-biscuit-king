079166ffad3d7d5c36861c1bdcd4369889ee4b89
Luis Garrillo <lggarrillo@urbe.edu.ve>
2025-08-28 22:20:29 -0400

    Implement judge into main scene

--- scripts/main.gd
+++ scripts/main.gd
@@ -3,6 +3,7 @@ extends Node2D
 
 @onready var current_round = 0
 @onready var game_over: GameOver = $GameOver
+@onready var judge: Judge = $Judge
 
 func _ready() -> void:
 	Data.load_dress_textures()
@@ -16,3 +17,16 @@ func start_round():
 
 func _on_dress_up_game_over() -> void:
 	game_over.animation_player.play("in")
+
+
+func _on_dress_up_judge_in(approved) -> void:
+	judge.set_up(approved)
+
+
+func _on_judge_game_over_judge() -> void:
+	dress_up.animation_player.play("game over")
+	_on_dress_up_game_over()
+
+
+func _on_judge_next_round() -> void:
+	dress_up.advance()
