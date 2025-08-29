92b520e62389cc5eb7fb71d45c4f7051cded9d85
Luis Garrillo <lggarrillo@urbe.edu.ve>
2025-08-28 23:42:58 -0400

    Implement title into main scene

--- scripts/main.gd
+++ scripts/main.gd
@@ -4,16 +4,18 @@ extends Node2D
 @onready var current_round = 0
 @onready var game_over: GameOver = $GameOver
 @onready var judge: Judge = $Judge
+@onready var title: Title = $Title
 
 func _ready() -> void:
 	Data.load_dress_textures()
-	dress_up.set_up_pieces()
-	dress_up.show_riddle()
+	title_in()
 	
-
 func start_round():
 	dress_up.set_up_pieces()
 	dress_up.show_riddle()
+	
+func title_in():
+	title.animation_player.play("in")
 
 func _on_dress_up_game_over() -> void:
 	game_over.animation_player.play("in")
@@ -30,3 +32,11 @@ func _on_judge_game_over_judge() -> void:
 
 func _on_judge_next_round() -> void:
 	dress_up.advance()
+
+
+func _on_title_start() -> void:
+	start_round()
+
+
+func _on_dress_up_finish_game() -> void:
+	title_in()
